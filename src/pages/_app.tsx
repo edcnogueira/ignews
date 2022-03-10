13cd5177b7c62b3eb73023653a1af72210a16213
Edjancarlos <edcnogueira@pop-os.localdomain>
2022-03-09 23:13:37 -0300

    feat: define o estilo global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import {AppProps} from "next/app"
 
+import "../styles/global.scss"
+
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
-import styles from "../styles/home.module.scss"
+import Head from "next/head"
+
 
 export default function Home() {
-  return <h1 className={styles.title}>Olá</h1>;
+  return (
+    <>
+      <Head>
+          <title>Início | ig.news</title>
+        </Head>
+      <h1>
+        Olá
+      </h1>
+    </>
+  )
 }
